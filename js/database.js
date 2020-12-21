db071b0aa6d4be4bce731cc4d584cf2f94f8ea59
Milahn <milahnmartin.develop@gmail.com>
2020-12-21 16:19:38 +0200

    added input form

---
+++ js/database.js
@@ -0,0 +1,15 @@
+// INPUT FORM 
+
+
+
+
+
+function getInputValue(){
+  // Selecting the input element and get its value 
+let mycontactfname = document.getElementById('fname').value;
+let mycontactlname = document.getElementById('lname').value;
+let mycontactage = document.getElementById('fage').value;
+
+
+};
+
